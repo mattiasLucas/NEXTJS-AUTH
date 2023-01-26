b4f86aa8e94c6047808639bff7bf5fb9f40d14aa
lawrenceagles <eaglescoder@gmail.com>
2023-01-26 14:46:17 +0000

    finished boilerplate

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,8 @@
 export default function Home() {
   return (
-    <section class="grid h-screen place-items-center">
+    <section className="grid h-screen place-items-center">
       <div>
-        <h2 class="text-center text-lg mb-4">Welcome To Login</h2>
-        <form id="add-book">
-          <div class="mx-auto max-w-lg ">
-            <div class="py-1">
-              <span class="px-1 text-sm text-gray-600">username</span>
-              <input id="username" placeholder="Enter your username" name="username" type="text"
-                class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
-            </div>
-
-            <div class="py-1">
-              <span class="px-1 text-sm text-gray-600">Password</span>
-              <input id="password" placeholder="Enter your password" name="password" type="text"
-                class="text-md block px-3 py-2 rounded-lg w-full bg-white border-2 border-gray-300 placeholder-gray-600 shadow-md focus:placeholder-gray-500 focus:bg-white focus:border-gray-600 focus:outline-none" />
-            </div>
-
-
-            <button id="create-book" type="submit"
-              class="mt-3 text-lg font-semibold bg-gray-800 w-full text-white rounded-lg px-6 py-3 block shadow-xl hover:text-white hover:bg-black">
-              submit
-            </button>
-          </div>
-        </form>
+        <h2>Welcome to LogRocket</h2>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,22 +5,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontSize: {
-      title: `2.6rem;`,
-      paragraph: `1.2rem;`
-    },
-    extend: {
-      colors: {
-        primary: {
-          500: '#FF6363;',
-          800: '#FF1313;',
-        }
-      },
-      fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        adelia: ["ADELIA", "cursive"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
